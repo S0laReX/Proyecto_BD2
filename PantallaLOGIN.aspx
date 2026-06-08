e4c83632787de91ae538b0e8d09fe70801e77bb9
S0laReX <santi19.nava.08@gmail.com>
2026-06-08 00:39:49 -0400

    f

--- PantallaLOGIN.aspx
+++ PantallaLOGIN.aspx
@@ -8,27 +8,8 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <section style="display: flex; justify-content: center;">
             <div>
-                <asp:Label ID="Label3" runat="server" Text="INICIO DE SESIÓN" style ="margin-left: 170px"></asp:Label>
-                <br />
-                <br />
-                <asp:Label ID="Label1" runat="server" Text="EMAIL" style="margin-left: 100px"></asp:Label>
-                <asp:TextBox ID="txtEmail" runat="server" style="margin-left: 100px"></asp:TextBox>
-                <br />
-                <br />
-                <asp:Label ID="Label2" runat="server" Text="CONTRASEÑA" style="margin-left: 100px"></asp:Label>
-                <asp:TextBox ID="txtContraseña" runat="server" style="margin-left: 46px"></asp:TextBox>
-                <br />
-                <br />
-                <asp:Label ID="lblMensaje" runat="server" Text="" style="margin-left: 140px"></asp:Label>
-                <br />
-                <asp:Button ID="btnIngresar" runat="server" Text="INGRESAR" style ="margin-left:123px" Width="112px" OnClick="btnIngresar_Click" />
-                <asp:Button ID="btnRegistro" runat="server" Text="REGISTRO" style ="margin-left:68px" Width="112px"/>
-    
-            </div>
-        </section>
-        
+            </div> 
     </form>
 </body>
 </html>
--- PantallaLOGIN.aspx.cs
+++ PantallaLOGIN.aspx.cs
@@ -10,82 +10,9 @@ namespace Proyecto_BDII
 {
     public partial class PantallaLOGIN : System.Web.UI.Page
     {
-        private string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["Mi Conexion"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        public string obtenerRol()
-        {
-            SqlConnection con = new SqlConnection(conexion);
-            string consulta = "SELECT rol FROM usuario WHERE correo = @correo AND contraseña = @pass";
-            SqlCommand cmd = new SqlCommand(consulta, con);
-            cmd.Parameters.AddWithValue("@correo", txtEmail.Text);
-            cmd.Parameters.AddWithValue("@pass", txtContraseña.Text);
-            con.Open();
-            string rol = cmd.ExecuteScalar().ToString();
-            con.Close();
-            return rol;
-        }
-
-        public bool EsValido()
-        {
-            
-            SqlConnection con = new SqlConnection(conexion);
-            string consulta = "SELECT COUNT(1) FROM usuario WHERE correo = @correo AND contraseña = @pass";
-            SqlCommand cmd = new SqlCommand(consulta, con);
-            cmd.Parameters.AddWithValue("@correo", txtEmail.Text);
-            cmd.Parameters.AddWithValue("@pass", txtContraseña.Text);
-            con.Open();
-            int num = Convert.ToInt32(cmd.ExecuteScalar());
-            Convert.ToInt32(cmd.ExecuteScalar());
-            con.Close();
-            if (num == 1)
-            {
-                
-              
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-            
-
-        }
-        protected void btnIngresar_Click(object sender, EventArgs e)
-        {
-            string email = txtEmail.Text;
-            string contraseña = txtContraseña.Text;
-            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(contraseña))
-            {
-                lblMensaje.Text = "Por favor, ingrese su correo electrónico y contraseña.";
-            }
-            else
-            {
-                if (EsValido())
-                {
-                    if (obtenerRol() == "admin")
-                    {
-                        Response.Redirect("PantallaADMIN.aspx");
-                    }
-                    else
-                    {
-                        Response.Redirect("PantallaUSER.aspx");
-                    }
-                    
-                }
-                else
-                {
-                    lblMensaje.Text = "Correo electrónico o contraseña incorrectos.";
-                }
-            }
-        }
-
-        protected void btnRegistro_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("PantallaREGISTRO.aspx");
         }
     }
 }
